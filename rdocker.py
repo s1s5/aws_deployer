96d56c6f859e3744cc19305d335487f012e369d9
Shogo Sawai <shogo.sawai@gmail.com>
2017-07-07 15:33:07 +0900

    use docker_tunnel module

--- rdocker.py
+++ rdocker.py
@@ -5,13 +5,19 @@ from __future__ import absolute_import
 import uuid
 import subprocess
 import os
+from multiprocessing import Process
 
-from docker_tools import over_ssh
+from docker_tools import docker_tunnel
 from fabric.state import env
 from six import print_
 
 env.forward_agent = True
 env.use_ssh_config = True
+
+
+def get_sock(hostname):
+    base = os.path.join('/tmp', 'docker-{}'.format(os.environ['USER']))
+    return 'unix://{}'.format(os.path.join(base, '{}.sock'.format(hostname)))
 
 
 def search_commons(hosts):
@@ -25,7 +31,7 @@ def search_commons(hosts):
     return common
 
 
-def create_rc_file(proxy, hosts, fp):
+def create_rc_file(hosts, fp):
     print_('if [ -e ~/.bash_profile ]; then . ~/.bash_profile; fi', file=fp)
     print_('if [ -e ~/.bashrc ]; then . ~/.bashrc; fi', file=fp)
 
@@ -35,19 +41,19 @@ def create_rc_file(proxy, hosts, fp):
     print_(r'export ORG_PS1="$PS1"', file=fp)
     if len(hosts) == 1:
         print_(r'export PS1="(->{}) $PS1"'.format(hosts[0]), file=fp)
-        print_('export DOCKER_HOST={}'.format(proxy.getSock(hosts[0])), file=fp)
+        print_('export DOCKER_HOST={}'.format(get_sock(hosts[0])), file=fp)
         print_('docker -> [{}]'.format(hosts[0]))
     else:
         common = search_commons(hosts)
         print_(r'export PS1="(rdocker) $PS1"', file=fp)
         for index, host in enumerate(hosts):
-            print_("alias docker_{}='docker -H {}'".format(host[len(common):], proxy.getSock(host)), file=fp)
-            print_("alias d{}='docker -H {}'".format(host[len(common):], proxy.getSock(host)), file=fp)
+            print_("alias docker_{}='docker -H {}'".format(host[len(common):], get_sock(host)), file=fp)
+            print_("alias d{}='docker -H {}'".format(host[len(common):], get_sock(host)), file=fp)
             print_('[{}] docker_{}(d{}) -> [{}]'.format(index, host[len(common):], host[len(common):], host))
         print_('function sw () {', file=fp)
         print_('    case $1 in', file=fp)
         for index, host in enumerate(hosts):
-            sock = proxy.getSock(host)
+            sock = get_sock(host)
             ps1 = 'export PS1="(->{})$ORG_PS1"'.format(host)
             print_('        {}) export DOCKER_HOST="{}"'.format(index, sock), file=fp)
             print_('            {} ;;'.format(ps1), file=fp)
@@ -59,22 +65,21 @@ def create_rc_file(proxy, hosts, fp):
         print_('}', file=fp)
 
 
-def main(args):
-    hosts = args
-    proxy = over_ssh.DockerMultipleProxy(
-        [str(x) for x in hosts],
-        "registry_default", sleep_time=1)
-    proxy.start()
+def main(options):
+    hosts = options.args
+
+    for hostname in hosts:
+        docker_tunnel.connect(hostname)
+
     tmp_filename = '/tmp/{}.sh'.format(uuid.uuid4().hex)
 
     with open(tmp_filename, 'w') as fp:
-        create_rc_file(proxy, hosts, fp)
+        create_rc_file(hosts, fp)
     # print tmp_filename
     # print open(tmp_filename, 'r').read()
     try:
         subprocess.call('/bin/bash --rcfile {}'.format(tmp_filename), shell=True)
     finally:
-        proxy.end()
         os.remove(tmp_filename)
 
 
@@ -85,7 +90,7 @@ def __entry_point():
     )
     parser.add_argument("args", nargs="*")
 
-    main(parser.parse_args().args)
+    main(parser.parse_args())
 
 
 if __name__ == '__main__':
